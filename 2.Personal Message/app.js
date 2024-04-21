7ffda64a46c0f176378bd0270f84db5f971f179f
iqraoffical <159756553+iqraoffical@users.noreply.github.com>
2024-04-22 04:23:12 +0500

    Add files via upload

---
+++ 2.Personal Message/app.js
@@ -0,0 +1,4 @@
+// Store a person’s name in a variable
+var personName = "Eric";
+// print a message to that person
+console.log("Hello ".concat(personName, ", would you like to learn some Python today?"));
---
+++ 2.Personal Message/app.ts.ts
@@ -0,0 +1,7 @@
+// Store a person’s name in a variable
+
+const personName: string = "Eric";
+
+// print a message to that person
+
+console.log(`Hello ${personName}, would you like to learn some Python today?`);
